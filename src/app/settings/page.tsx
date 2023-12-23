6f22b3f49b8ad65d5d383ff0aafc6ffeca4d796b
Yuri Souza <yurdesou@gmail.com>
2023-12-23 14:27:19 -0300

    Correct OrderPanel folder name

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <main className='flex flex-col gap-6 bg-base-dark-bg-1 p-6 pl-[128px] pr-[434px]'>
+      <h1 className='text-heading-h1 text-white'>Settings</h1>
+    </main>
+  );
+};
+
+export default Settings;
