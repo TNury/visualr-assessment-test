7e32e6c73b6a8bd0ca08735d67ab48b72ca3f53c
Yuri Souza <yurdesou@gmail.com>
2023-12-23 17:51:36 -0300

    Create route for Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-const Settings = () => {
+const Dashboard = () => {
   return (
     <main className='flex flex-col gap-6 bg-base-dark-bg-1 p-6 pl-[128px] pr-[434px]'>
-      <h1 className='text-heading-h1 text-white'>Settings</h1>
+      <h1 className='text-heading-h1 text-white'>Dashboard</h1>
     </main>
   );
 };
 
-export default Settings;
+export default Dashboard;
