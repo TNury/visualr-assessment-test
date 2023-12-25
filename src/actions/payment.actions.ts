83dd174546b8f4ddb44424c3751c0c43bb8ddb50
Yuri Souza <yurdesou@gmail.com>
2023-12-25 03:37:39 -0300

    Update Stripe secret key environment variable

--- src/actions/payment.actions.ts
+++ src/actions/payment.actions.ts
@@ -1,10 +1,10 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.stripeSk);
+const stripe = new Stripe(process.env.NEXT_PUBLIC_stripeSk);
 
 export async function createPaymentIntent(amount: number) {
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount,
+    amount: amount * 100,
     currency: 'usd',
   });
 
