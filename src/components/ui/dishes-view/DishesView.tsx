bc764c098968b195e3dc75b8546968d0ca9842c2
Yuri Souza <yurdesou@gmail.com>
2023-12-25 14:11:52 -0300

    Update grid layout in DishesView component

--- src/components/ui/dishes-view/DishesView.tsx
+++ src/components/ui/dishes-view/DishesView.tsx
@@ -12,7 +12,7 @@ export const DishesView: React.FC<DishesViewProps> = async ({ activeMenu }) => {
   return (
     <div className='flex flex-col gap-6'>
       <h2 className='text-heading-h2 text-white'>Choose Dishes</h2>
-      <div className='row-gap grid w-full grid-cols-3 gap-x-7 gap-y-6'>
+      <div className='row-gap grid w-full grid-cols-3 gap-x-7 gap-y-6 xl:grid-cols-6'>
         {response.data.menu.data.attributes.dishes.data.map((entry) => (
           <DishCard dishProps={entry} key={entry.id} />
         ))}
