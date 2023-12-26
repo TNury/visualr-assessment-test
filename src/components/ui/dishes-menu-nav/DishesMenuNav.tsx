1fb6f713630f7a4a08710a352d2557cf5330d741
Yuri Souza <yurdesou@gmail.com>
2023-12-26 18:04:18 -0300

    Rename MenuNav to DishesMenuNav, make it reusable

--- src/components/ui/dishes-menu-nav/DishesMenuNav.tsx
+++ src/components/ui/dishes-menu-nav/DishesMenuNav.tsx
@@ -2,32 +2,38 @@ import Link from 'next/link';
 
 import { getMenusData } from '@vat/actions/menu.actions';
 
-type MenuNavProps = {
+type DishesMenuNav = {
   activeMenu: string;
+  linkPathname: string;
 };
 
-export const MenuNav: React.FC<MenuNavProps> = async ({ activeMenu }) => {
+export const DishesMenuNav: React.FC<DishesMenuNav> = async ({
+  activeMenu,
+  linkPathname,
+}) => {
   const menusData = await getMenusData();
 
   return (
     <nav className='h-fit w-full'>
-      <ul className='relative flex gap-8'>
+      <ul className='flex gap-8'>
         {menusData.data.menus.data.map((entry, index) => (
           <li key={index} className='z-10'>
             <Link
               data-active={entry.id === activeMenu}
-              href={`/?menu=${entry.id}`}
+              href={{
+                pathname: linkPathname,
+                query: { menu: entry.id },
+              }}
               key={index}
-              className='flex flex-col gap-3 text-body-base-semibold text-white data-[active="true"]:text-primary'>
+              className='relative flex flex-col pb-3 text-body-base-semibold text-white data-[active="true"]:text-primary'>
               {entry.attributes.title}
 
               {entry.id === activeMenu && (
-                <div className='h-[3px] w-1/2 rounded-sm bg-primary' />
+                <div className='absolute bottom-[-2px] left-0 h-[3px] w-3/5 rounded-sm bg-primary' />
               )}
             </Link>
           </li>
         ))}
-        <div className='absolute bottom-[1.1px] left-0 h-[1px] w-full bg-base-dark-line' />
       </ul>
     </nav>
   );
