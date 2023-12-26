9cdf54cc2e948546987016baa68dc1a784a1b6b1
Yuri Souza <yurdesou@gmail.com>
2023-12-26 18:05:15 -0300

    Rename header to homepage header

--- src/components/ui/homepage-header/HomepageHeader.tsx
+++ src/components/ui/homepage-header/HomepageHeader.tsx
@@ -7,9 +7,9 @@ type HeaderProps = {
 };
 
 // @TODO - CORRECT ARBRITRARY VALUES
-export const Header: React.FC<HeaderProps> = ({ children }) => {
+export const HomepageHeader: React.FC<HeaderProps> = ({ children }) => {
   return (
-    <header className='sticky top-0 z-40 flex flex-col gap-6 bg-base-dark-bg-1 pt-6'>
+    <header className='sticky top-0 z-40 flex flex-col gap-6 border-b border-base-dark-line bg-base-dark-bg-1 pt-6'>
       <div className='flex w-full items-center justify-between'>
         <div className='flex w-full flex-col justify-start gap-1'>
           <h1 className='text-heading-h1 text-white'>Jaegar Resto</h1>
