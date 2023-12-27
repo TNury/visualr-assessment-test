74a6221b5e9318c2dd30d332cdaf95ffaf675a43
Yuri Souza <yurdesou@gmail.com>
2023-12-26 23:40:54 -0300

    Refactor Drawer component to remove unnecessary code and add urlToReturnTo prop

--- src/components/ui/drawer/Drawer.tsx
+++ src/components/ui/drawer/Drawer.tsx
@@ -1,37 +1,31 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 
 type DrawerProps = {
-  open: boolean;
   children: React.ReactNode;
+  urlToReturnTo: LinkProps['href'];
 };
 
-export const Drawer: React.FC<DrawerProps> = ({ open, children }) => {
+export const Drawer: React.FC<DrawerProps> = ({ children, urlToReturnTo }) => {
   useEffect(() => {
     const bodyRef = document.querySelector('body');
 
-    if (open) {
-      bodyRef.classList.add('overflow-hidden');
-    }
+    bodyRef.classList.add('overflow-hidden');
 
     return () => {
       bodyRef.classList.remove('overflow-hidden');
     };
-  }, [open]);
-
-  if (!open) return null;
+  }, []);
 
   return (
-    <div
-      data-open={open}
-      className='group fixed right-0 top-0 z-50 flex h-screen w-screen flex-col items-end rounded-l-lg data-[open="false"]:pointer-events-none'>
-      <Link href='/' scroll={false}>
-        <div className='animate-fade-in absolute inset-0 -z-10 bg-base-overlay' />
+    <div className='group fixed right-0 top-0 z-50 flex h-screen w-screen flex-col items-end rounded-l-lg'>
+      <Link href={urlToReturnTo} scroll={false}>
+        <div className='absolute inset-0 -z-10 animate-fade-in bg-base-overlay' />
       </Link>
-      <div className='z-60 animate-slide-in-left h-screen w-fit'>
+      <div className='z-60 h-screen w-fit animate-slide-in-left'>
         {children}
       </div>
     </div>
