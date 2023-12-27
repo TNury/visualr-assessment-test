54bf122965190aff495bd03eed47c37fd464820b
Yuri Souza <yurdesou@gmail.com>
2023-12-27 19:20:56 -0300

    Refactor Snackbar component styles, add bg color to its root

--- src/components/ui/snackbar/Snackbar.tsx
+++ src/components/ui/snackbar/Snackbar.tsx
@@ -8,22 +8,19 @@ import { cn } from '@vat/lib/utils';
 
 import { cva } from 'class-variance-authority';
 
-const snackbarVariants = cva(
-  'fixed rounded-lg z-[99] bottom-4 animate-fade-in left-32 p-[14px] text-white',
-  {
-    variants: {
-      variant: {
-        info: 'bg-blue-500',
-        error: 'bg-accents-bg-red text-accents-red',
-        success: 'bg-accents-bg-green text-accents-green',
-        warning: 'bg-yellow-500',
-      },
+const snackbarVariants = cva('p-3.5', {
+  variants: {
+    variant: {
+      info: 'bg-blue-500',
+      error: 'bg-accents-bg-red text-accents-red',
+      success: 'bg-accents-bg-green text-accents-green',
+      warning: 'bg-yellow-500',
     },
-    defaultVariants: {
-      variant: 'info',
-    },
-  }
-);
+  },
+  defaultVariants: {
+    variant: 'info',
+  },
+});
 
 export const Snackbar = () => {
   const { snackbarState, dispatch } = useSnackbarContext();
@@ -40,8 +37,10 @@ export const Snackbar = () => {
   if (!severity) return null;
 
   return (
-    <div className={cn(snackbarVariants({ variant: severity }))}>
-      <p className='text-body-lg-medium'>{message}</p>
+    <div className='fixed bottom-4 left-32 z-[99] animate-fade-in overflow-clip rounded-lg bg-base-dark-bg-2 text-white'>
+      <div className={cn(snackbarVariants({ variant: severity }))}>
+        <p className='text-body-lg-medium'>{message}</p>
+      </div>
     </div>
   );
 };
