b73b089ca0c1709bde9078dab660a93297a703b1
Yuri Souza <yurdesou@gmail.com>
2023-12-28 03:45:24 -0300

    Implement search functionality on Dishes ManagementView

--- src/app/settings/products-management/page.tsx
+++ src/app/settings/products-management/page.tsx
@@ -1,31 +1,31 @@
 import { DishCreationDrawer } from '@vat/components/ui/dish-creation-drawer/DishCreationDrawer';
 import { DishesManagementView } from '@vat/components/ui/dishes-management-view/DishesManagementView';
 import { DishesMenuNav } from '@vat/components/ui/dishes-menu-nav/DishesMenuNav';
+import { ProductsManagementHeader } from '@vat/components/ui/products-management-header/ProductsManagementHeader';
 
 type ProductsManagementProps = {
   params: {};
   searchParams: {
-    menu: string;
-
-    openDishCreationDrawerOnMenu: string;
+    menu?: string;
+    search?: string;
+    openDishCreationDrawerOnMenu?: string;
   };
 };
 
 const ProductsManagement: React.FC<ProductsManagementProps> = (props) => {
   const activeMenu = props.searchParams.menu;
+  const searchQuery = props.searchParams.search;
 
   const openDishCreationDrawer =
     props.searchParams.openDishCreationDrawerOnMenu;
 
   return (
     <div className='flex h-full w-full flex-col'>
-      <div className='flex p-6'>
-        <h2 className='text-heading-h2 text-white'>Products Management</h2>
-      </div>
+      <ProductsManagementHeader />
       <div className='border-b border-base-dark-line px-6'>
         <DishesMenuNav activeMenu={activeMenu} />
       </div>
-      <DishesManagementView activeMenu={activeMenu} />
+      <DishesManagementView searchQuery={searchQuery} activeMenu={activeMenu} />
 
       {Boolean(openDishCreationDrawer) && (
         <DishCreationDrawer
--- src/components/ui/dishes-management-view/DishesManagementView.tsx
+++ src/components/ui/dishes-management-view/DishesManagementView.tsx
@@ -4,7 +4,17 @@ import { Button } from '@vat/components/ui/button/Button';
 import { DishManagementCard } from '@vat/components/ui/dish-management-card/DishManagementCard';
 import { Add } from '@vat/components/ui/icons/Add';
 
-import { getMenuById } from '@vat/actions/menu.actions';
+import {
+  getDishesBySearchString,
+  getMenuById,
+} from '@vat/actions/menu.actions';
+
+import {
+  GetDishesBySearchStringResponse,
+  GetMenuByIdResponse,
+} from '@vat/types/menu.types';
+
+import { PlaceholderMessage } from '../placeholder-message/PlaceholderMessage';
 
 type DishesManagementViewProps = {
   activeMenu: string;
@@ -14,34 +24,52 @@ type DishesManagementViewProps = {
 export const DishesManagementView: React.FC<
   DishesManagementViewProps
 > = async ({ searchQuery, activeMenu }) => {
-  const menuResponse = await getMenuById({ id: activeMenu });
+  let results:
+    | GetMenuByIdResponse['data']['menu']['data']['attributes']['dishes']['data']
+    | GetDishesBySearchStringResponse['data']['dishes']['data'];
 
-  return (
+  let menuTitle: string;
+
+  if (searchQuery) {
+    const searchResponse = await getDishesBySearchString({
+      searchString: searchQuery,
+    });
+
+    results = searchResponse.data.dishes.data;
+  } else {
+    const menuResponse = await getMenuById({ id: activeMenu });
+
+    menuTitle = menuResponse.data.menu.data.attributes.title;
+    results = menuResponse.data.menu.data.attributes.dishes.data;
+  }
+
+  return searchQuery && results.length === 0 ? (
+    <PlaceholderMessage
+      title='No dishes found'
+      body='Try searching for something else'
+      bodySize='lg'
+    />
+  ) : (
     <div className='grid w-full flex-1 auto-rows-[300px] grid-cols-3 gap-4 overflow-auto p-6 lg:grid-cols-4 xl:grid-cols-6'>
-      <div className='relative flex flex-col items-center justify-center gap-2 rounded-lg border border-dashed border-primary'>
-        <Link
-          href={{
-            query: {
-              menu: activeMenu,
-              openDishCreationDrawerOnMenu:
-                menuResponse.data.menu.data.attributes.title,
-            },
-          }}>
-          <Button variant='ghost' className='w-fit' role='none' tabIndex={-1}>
-            <Add className='h-5 w-5' />
-          </Button>
-        </Link>
-        <p className='text-body-lg-semibold text-primary'>Add new dish</p>
-      </div>
-      {menuResponse.data.menu.data.attributes.dishes.data.map(
-        (entry, index) => (
-          <DishManagementCard
-            key={index}
-            menuTitle={menuResponse.data.menu.data.attributes.title}
-            dishProps={entry}
-          />
-        )
+      {!searchQuery && (
+        <div className='relative flex flex-col items-center justify-center gap-2 rounded-lg border border-dashed border-primary'>
+          <Link
+            href={{
+              query: {
+                menu: activeMenu,
+                openDishCreationDrawerOnMenu: menuTitle,
+              },
+            }}>
+            <Button variant='ghost' className='w-fit' role='none' tabIndex={-1}>
+              <Add className='h-5 w-5' />
+            </Button>
+          </Link>
+          <p className='text-body-lg-semibold text-primary'>Add new dish</p>
+        </div>
       )}
+      {results.map((entry, index) => (
+        <DishManagementCard key={index} dishProps={entry} />
+      ))}
     </div>
   );
 };
