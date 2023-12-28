ef7b330fd1a0215dc3cd08cd7b05ba4a4c72694a
Yuri Souza <yurdesou@gmail.com>
2023-12-28 16:14:48 -0300

    Refactor DishCreationForm component to levarage ReturnLink

--- src/components/ui/dish-creation-form/DishCreationForm.tsx
+++ src/components/ui/dish-creation-form/DishCreationForm.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import Link from 'next/link';
-
 import { Button } from '@vat/components/ui/button/Button';
 import { MediaField } from '@vat/components/ui/media-field/MediaField';
+import { ReturnLink } from '@vat/components/ui/return-link/ReturnLink';
 import { TextField } from '@vat/components/ui/text-field/TextField';
 
 import { useDishCreationForm } from './useDishCreationForm';
@@ -15,8 +14,13 @@ type DishCreationFormProps = {
 export const DishCreationForm: React.FC<DishCreationFormProps> = ({
   menuId,
 }) => {
-  const { formik, handleMediaAddition, handlePriceChange, handlePriceOnBlur } =
-    useDishCreationForm({ menuId });
+  const {
+    formik,
+    handleMediaAddition,
+    handleMediaRemoval,
+    handlePriceChange,
+    handlePriceOnBlur,
+  } = useDishCreationForm({ menuId });
 
   return (
     <form
@@ -29,6 +33,7 @@ export const DishCreationForm: React.FC<DishCreationFormProps> = ({
             label='Media'
             error={formik.touched.media && (formik.errors.media as string)}
             addMedia={handleMediaAddition}
+            removeMedia={handleMediaRemoval}
           />
 
           <TextField
@@ -52,13 +57,9 @@ export const DishCreationForm: React.FC<DishCreationFormProps> = ({
       </div>
 
       <div className='mx-6 flex gap-2 border-t border-base-dark-line pt-6'>
-        <Link
-          href={{
-            pathname: '/settings/products-management',
-          }}
-          className='pointer-events-auto w-full'>
+        <ReturnLink>
           <Button variant='outlined'>Cancel</Button>
-        </Link>
+        </ReturnLink>
         <Button type='submit'>Create dish</Button>
       </div>
     </form>
