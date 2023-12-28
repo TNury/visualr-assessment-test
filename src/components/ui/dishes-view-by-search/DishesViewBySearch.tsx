8308e2e4e48958cb2b6f449fe07beb12c1165530
Yuri Souza <yurdesou@gmail.com>
2023-12-28 03:41:36 -0300

    Remove unecessary comments from DishesViewBySearch

--- src/components/ui/dishes-view-by-search/DishesViewBySearch.tsx
+++ src/components/ui/dishes-view-by-search/DishesViewBySearch.tsx
@@ -7,11 +7,6 @@ type DishesViewBySearchProps = {
   searchQuery: string;
 };
 
-// Also, add messag for when theres no items on menu??
-// Also, sort dishes by created at date
-// Also, add dishes by search thingy i think
-// Also, you you search for a dish, maybe it shouldnt show the menu highlighted??
-// Have a look at the border dash
 export const DishesViewBySearch: React.FC<DishesViewBySearchProps> = async ({
   searchQuery,
 }) => {
