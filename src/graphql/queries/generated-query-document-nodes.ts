2afe3ea2bc353943f40a5a5375b637670e4e5c5b
Yuri Souza <yurdesou@gmail.com>
2023-12-29 13:06:01 -0300

    Update paginatedOrderReportByDate to sort by most recent orders first

--- src/graphql/queries/generated-query-document-nodes.ts
+++ src/graphql/queries/generated-query-document-nodes.ts
@@ -211,7 +211,11 @@ export const RawDashboardHighlightsByDateRange = gql`
     `;
 export const PaginatedOrderReportByDate = gql`
     query paginatedOrderReportByDate($pagination: PaginationArg!, $date: DateTimeFilterInput!) {
-  orders(pagination: $pagination, filters: {createdAt: $date}) {
+  orders(
+    pagination: $pagination
+    filters: {createdAt: $date}
+    sort: "createdAt:desc"
+  ) {
     data {
       id
       attributes {
